79779d3308e07d91bcd80551e8046993df0bdaba
“MichalMazuz” <michal7969@gmail.com>
2025-08-17 20:38:27 +0300

    הוספתי את קבצי React כתיקייה רגילה

---
+++ ToDoListReact/src/service.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://localhost:5291"
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    console.error("API Error:", error);
+    return Promise.reject(error);
+  }
+);
+
+export default {
+  getTasks: async () => {
+    const result = await axios.get(`/items`)
+    return result.data;
+  },
+
+  addTask: async (name) => {
+    const result = await axios.post(`/items`, { name, isComplete: false });
+    return result.data;
+  },
+
+  setCompleted: async (id, updateItem) => {
+    const result = await axios.put(`/item/${id}`, updateItem);
+    return result.data;
+  },
+
+  deleteTask: async (id) => {
+    const result = await axios.delete(`/item/${id}`, id);
+    return result.data;
+  }
+};
